bc4d2bc1cac44ca14aae1b73ff061a5a20ec263d
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-01 23:14:16 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/miguel/zelda/ZeldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.zelda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
