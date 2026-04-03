498956434f4d823349003dfde21fb7b0daaa954c
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-03 18:43:58 -0300

    Adiciona metodos de encontrar por nome e encontrar por ultimo jogo lançado

--- src/main/java/com/miguel/zelda/repository/GameRepository.java
+++ src/main/java/com/miguel/zelda/repository/GameRepository.java
@@ -3,5 +3,9 @@ package com.miguel.zelda.repository;
 import com.miguel.zelda.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GameRepository extends JpaRepository<Game, String> {
+    Optional<Game> findByName(String name);
+    Optional<Game> findTopByOrderByReleasedDateDesc();
 }
