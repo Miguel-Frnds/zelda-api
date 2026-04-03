a0a0d5ff79495d7ee8cb337eb34cfcd35d7ea411
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-03 17:22:26 -0300

    Cria DTO para receber dados da API

---
+++ src/main/java/com/miguel/zelda/dto/api/GameApiDTO.java
@@ -0,0 +1,13 @@
+package com.miguel.zelda.dto.api;
+
+import lombok.Getter;
+
+@Getter
+public class GameApiDTO {
+    private String id;
+    private String name;
+    private String description;
+    private String developer;
+    private String publisher;
+    private String releasedDate;
+}
