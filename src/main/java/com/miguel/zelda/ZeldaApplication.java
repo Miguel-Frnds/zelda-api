7a75da97d3ea0b4544cfedb0e5c4ac9e8ed8dbe1
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-03 17:29:39 -0300

    Adiciona Annotation para ativar o OpenFeign

--- src/main/java/com/miguel/zelda/ZeldaApplication.java
+++ src/main/java/com/miguel/zelda/ZeldaApplication.java
@@ -2,8 +2,10 @@ package com.miguel.zelda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ZeldaApplication {
 
 	public static void main(String[] args) {
