9811bf718335ac58fbfd032260fee8f05b08167e
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-03 18:48:13 -0300

    Adiciona JsonProperty para mapear campo released_date da API

--- src/main/java/com/miguel/zelda/dto/api/GameApiDTO.java
+++ src/main/java/com/miguel/zelda/dto/api/GameApiDTO.java
@@ -1,5 +1,6 @@
 package com.miguel.zelda.dto.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
@@ -9,5 +10,7 @@ public class GameApiDTO {
     private String description;
     private String developer;
     private String publisher;
+
+    @JsonProperty("released_date")
     private String releasedDate;
 }
